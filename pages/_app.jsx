ab5412f2a22c9726d3f262146ce06197886dd610
George Phillips <george@cloudcannon.com>
2021-09-10 17:12:01 +1200

    added live editing

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,7 @@
 import '../styles/main.scss';
+import { CloudCannonConnect } from '@cloudcannon/react-connector'
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	const AppComponent = CloudCannonConnect(Component);
+	return <AppComponent {...pageProps}/>
 }
